a3ad71e46407dd356fd6fd009649071a2a3c3946
hnmahamud <hnmahamud@gmail.com>
2023-05-21 06:36:09 +0600

    update toy update page

--- src/pages/UpdateToy/UpdateToy.jsx
+++ src/pages/UpdateToy/UpdateToy.jsx
@@ -1,14 +1,16 @@
-import { toast } from "react-toastify";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { useState } from "react";
 import LoadingSpinner from "../Shared/LoadingSpinner/LoadingSpinner";
 import useTitle from "../../hooks/useTitle";
+import Hero from "../Shared/Hero/Hero";
+import Swal from "sweetalert2";
 
 const UpdateToy = () => {
   // Custom hook for set website page wise title
   useTitle("Update Toy");
   const { id } = useParams();
+  const [nLoading, setNLoading] = useState(false);
   const [toy, setToy] = useState();
   const navigate = useNavigate();
 
@@ -24,6 +26,7 @@ const UpdateToy = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setNLoading(true);
     const form = event.target;
     const price = form.price.value;
     const availableQuantity = form.availableQuantity.value;
@@ -44,17 +47,13 @@ const UpdateToy = () => {
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         if (data.modifiedCount > 0) {
-          toast("Toy update successful!", {
-            position: "top-center",
-            autoClose: 3000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "light",
+          setNLoading(false);
+          Swal.fire({
+            title: "Success!",
+            text: "Toy update successful!",
+            icon: "success",
+            confirmButtonText: "Cool",
           });
           navigate("/my-toys", { replace: true });
         }
@@ -67,11 +66,9 @@ const UpdateToy = () => {
   }
 
   return (
-    <div className="md:h-[calc(100vh-134px)] my-8">
-      <h1 className="text-center text-2xl text-gray-500 font-extrabold mb-8 md:mb-16">
-        Update toy
-      </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+    <div className="my-8">
+      <Hero title="Update toy"></Hero>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-8">
         <div>
           <img
             className="md:h-[450px] rounded-md"
@@ -124,11 +121,35 @@ const UpdateToy = () => {
               </div>
 
               <div className="form-control mt-4">
-                <button className="bg-gray-100 w-full text-blue-600 font-semibold border rounded-full hover:bg-gray-200">
-                  <div className="flex justify-center items-center space-x-4 p-2">
-                    Update
-                  </div>
-                </button>
+                {nLoading ? (
+                  <button className="bg-gray-100 w-full rounded-full text-blue-600 border border-gray-200 p-2">
+                    <div role="status">
+                      <svg
+                        aria-hidden="true"
+                        className="inline w-6 h-6 text-gray-200 animate-spin fill-blue-600"
+                        viewBox="0 0 100 101"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                          fill="currentColor"
+                        />
+                        <path
+                          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                          fill="currentFill"
+                        />
+                      </svg>
+                      <span className="sr-only">Loading...</span>
+                    </div>
+                  </button>
+                ) : (
+                  <button className="bg-gray-100 w-full text-blue-600 font-semibold border rounded-full hover:bg-gray-200">
+                    <div className="flex justify-center items-center space-x-4 p-2">
+                      Update
+                    </div>
+                  </button>
+                )}
               </div>
             </form>
           </div>
